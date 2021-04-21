8139c875977da369b741b5202b798b0e86a20cf8
Jesse Crouch <jessecrouch@canusa-apc.com>
2021-04-21 08:21:24 -0400

    Added json web tokens and login verification for security

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,21 +3,30 @@ import $ from 'jquery';
 import User from './User';
 import { uuid } from 'uuidv4';
 import extensions from '../fetchExtensions';
+import Cookies from 'js-cookie';
+
+const server = 'http://10.5.1.57:5650';
 
 export default class Main extends Component {
     constructor() {
         super();
+
+        this.fetchUsers = this.fetchUsers.bind(this);
+        this.filterUsers = this.filterUsers.bind(this);
 
         this.state = {
             users: []
         }
     }
 
-    componentDidMount() {
-        $.get('http://10.5.1.57:5650/getAllUsers', result => {
+    fetchUsers() {
+        $.post(server + '/getAllUsers', { token: Cookies.get('token') }, result => {
             if (result.error) {
                 alert(result.error);
             } else {
+                document.getElementById('login-content').style.display = 'none';
+                document.getElementById('main-content').style.display = '';
+
                 // Filter for actual users
                 var users = [];
                 for (var i in result.users) {
@@ -47,32 +56,84 @@ export default class Main extends Component {
         });
     }
 
+    componentDidMount() {
+        // Check for token cookie
+        if (Cookies.get('token')) {
+            this.fetchUsers();
+        } else {
+            document.getElementById('login-content').style.display = '';
+        }
+    }
+
+    filterUsers(name, email, location, extension) {
+        var users = this.state.allUsers;
+        users = users.filter(user => {
+            var match = false;
+            if (name && user.props.user.DISPLAY_NAME.toLowerCase().includes(name.toLowerCase())) match = true;
+            if (email && user.props.user.EMAIL.toLowerCase().includes(email.toLowerCase())) match = true;
+            if (location && user.props.user.OU_NAME.toLowerCase().includes(location.toLowerCase())) match = true;
+            if (extension && user.props.user.EXTENSION.toLowerCase().includes(extension.toLowerCase())) match = true;
+
+            if (!name && !email && !location && !extension) {
+                return true;
+            } else {
+                return match;
+            }
+        });
+        this.setState(prevState => {
+            return {
+                allUsers: prevState.allUsers,
+                users: users
+            }
+        });
+    }
+
     render() {
         return (
             <div style={{textAlign: 'center'}}>
                 <h3 style={{margin: '2%'}}>Employee Directory</h3>
-                <input className="form-control" id="searchField" placeholder="Search" style={{width: '20%', margin: '1% auto'}} onChange={e => {
-                    var users = this.state.allUsers;
-                    users = users.filter(user => {
-                        var match = false;
-                        if (user.props.user.DISPLAY_NAME.toLowerCase().includes(e.target.value.toLowerCase())) match = true;
-                        if (user.props.user.EMAIL_ADDRESS.toLowerCase().includes(e.target.value.toLowerCase())) match = true;
-                        if (user.props.user.OU_NAME.toLowerCase().includes(e.target.value.toLowerCase())) match = true;
-                        if (user.props.user.POSITION.toLowerCase().includes(e.target.value.toLowerCase())) match = true;
-                        if (user.props.user.EXTENSION.toLowerCase().includes(e.target.value.toLowerCase())) match = true;
-
-                        return match;
-                    });
-                    this.setState(prevState => {
-                        return {
-                            allUsers: prevState.allUsers,
-                            users: users
-                        }
-                    });
-                }} />
-                <h5 id="loading-text">Loading...</h5>
-                <div id="users">
-                    {this.state.users}
+                <div id="login-content" style={{display: 'none'}}>
+                    <div className="form-group">
+                        <input id="usernameInput" value="admin" className="form-control" placeholder="Username" style={{width: '20%', margin: '1% auto'}} />
+                    </div>
+                    <div className="form-group">
+                        <input id="passwordInput" value="canusa" className="form-control" placeholder="Password" type="password" style={{width: '20%', margin: '1% auto'}} />
+                    </div>
+                    <button className="btn btn-primary" onClick={() => {
+                        // Login
+                        $.post(server + '/login', {
+                            username: document.getElementById('usernameInput').value,
+                            password: document.getElementById('passwordInput').value
+                        }, result => {
+                            if (result.error) {
+                                alert(result.error);
+                            } else {
+                                // Set json web token cookie, expires in 8 hours
+                                Cookies.set('token', result.token, { expires: 0.34 });
+                                this.fetchUsers();
+                            }
+                        });
+                    }}>Login</button>
+                </div>
+                <div id="main-content" style={{display: 'none'}}>
+                    <div id="search-fields">
+                        <input className="form-control" id="nameSearchField" placeholder="Name" style={{width: '20%', margin: '1% auto'}} onChange={e => {
+                            this.filterUsers(document.getElementById('nameSearchField').value, null, null, null);
+                        }} />
+                        <input className="form-control" id="emailSearchField" placeholder="Email" style={{width: '20%', margin: '1% auto'}} onChange={e => {
+                            this.filterUsers(null, document.getElementById('emailSearchField').value, null, null);
+                        }} />
+                        <input className="form-control" id="locationSearchField" placeholder="Location" style={{width: '20%', margin: '1% auto'}} onChange={e => {
+                            this.filterUsers(null, null, document.getElementById('locationSearchField').value, null);
+                        }} />
+                        <input className="form-control" id="extensionSearchField" placeholder="Extension" style={{width: '20%', margin: '1% auto'}} onChange={e => {
+                            this.filterUsers(null, null, null, document.getElementById('extensionSearchField').value);
+                        }} />
+                    </div>
+                    <h5 id="loading-text">Loading...</h5>
+                    <div id="users">
+                        {this.state.users}
+                    </div>
                 </div>
             </div>
         )
